92992bd64f8009c13618400045cf6993897346b7
李家朋 <accounts_5c921fd1bb0faf00185f2dd2@mail.teambition.com>
2021-01-26 18:13:59 +0800

    fix: 合并

--- src/services/blog.js
+++ src/services/blog.js
@@ -8,11 +8,19 @@ module.exports = {
     await Blog.destroy({ where: { id } })
   },
   detail: async (id) => {
-    let data = await Blog.findOne({
+    let { dataValues: data } = await Blog.findOne({
+      include: [
+        {
+          model: Cate,
+          attributes: ['id', 'name'],
+        },
+      ],
       where: {
         id,
       },
     })
+    data.createdAt = moment(data.createdAt).format('YYYY-MM-DD HH:mm:ss')
+    data.updatedAt = moment(data.updatedAt).format('YYYY-MM-DD HH:mm:ss')
     return data
   },
   update: async ({ title, cateId, content, id }) => {
@@ -35,7 +43,7 @@ module.exports = {
   list: async ({ page = 1, per_page = 12, title = '' }) => {
     let { count: total, rows } = await Blog.findAndCountAll({
       order: [['createdAt', 'DESC']],
-      attributes: ['id', 'title', 'cateId', 'content', 'createdAt'],
+      attributes: ['id', 'title', 'cateId', 'content', 'createdAt', 'updatedAt'],
       offset: (page - 1) * per_page,
       limit: per_page,
       where: {
@@ -54,6 +62,7 @@ module.exports = {
       list: rows.map((item) => ({
         ...item.dataValues,
         createdAt: moment(item.dataValues.createdAt.getTime()).format('YYYY-MM-DD HH:mm:ss'),
+        updatedAt: moment(item.dataValues.updatedAt.getTime()).format('YYYY-MM-DD HH:mm:ss'),
       })),
       total,
     }
