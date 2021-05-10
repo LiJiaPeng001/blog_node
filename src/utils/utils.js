48924dd0271248d9941a59f5404d3e45ff0f9867
LiJiaPeng001 <1146417576@qq.com>
2021-05-10 16:36:36 +0800

    fix: 合并

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,8 +10,10 @@ const SECRET_KET = "lijiapeng_521"; // 加密密钥
 
 const maxAge = 60 * 60 * 24 * 7;
 
+console.log(process.env.NODE_ENV, "env");
+
 const uploadImageUrl =
-  process.env.NODE_ENV === "development" ? "/etc" : "/var/www/static/images";
+  process.env.NODE_ENV === "development" ? "./" : "/var/www/static/images/";
 
 module.exports = {
   errorMessage,
